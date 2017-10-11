aac0a92ca352da1a5be534beb0c1a59ed0fd15b6
Stephane SIMO <stephanedarcy.simomba@epitech.eu>
2017-10-11 22:19:18 +0900

    Initial Commit

---
+++ V-Labs-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  V-Labs-Test
+//
+//  Created by Stephane Darcy SIMO MBA on 11/10/2017.
+//  Copyright © 2017 v-Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ V-Labs-TestTests/V_Labs_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  V_Labs_TestTests.swift
+//  V-Labs-TestTests
+//
+//  Created by Stephane Darcy SIMO MBA on 11/10/2017.
+//  Copyright © 2017 v-Labs. All rights reserved.
+//
+
+import XCTest
+@testable import V_Labs_Test
+
+class V_Labs_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
